aab7468f36aa2134c66c077288632c43b94be292
Luis Pena <lpenapec@gmail.com>
2025-03-16 12:12:12 -0500

    Fixed Question

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
+import BaseTitle from '@/components/BaseTitle.vue';
 
 const api = useAPI()
 const question = ref(null)
@@ -14,5 +15,13 @@ onMounted(async () => {
 </script>
 
 <template>
-{{ question }}
+
+<div v-if="question" class="">
+<BaseTitle>{{  question.category }}</BaseTitle>
+{{  question.question  }}
+</div>
+<div v-else class="">
+
+</div>
+
 </template>
